249b25d922dcf7c60b7b5a1311c6f9df577a880b
Mohammad Baker <mohamadmbaker@gmail.com>
2025-03-16 19:36:39 +0100

    Safer user registration requirements

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -1,11 +1,18 @@
 const { check, validationResult } = require("express-validator");
 
 const validateUserRegistration = [
-  check("name").not().isEmpty().withMessage("Name is required"),
-  check("email").isEmail().withMessage("Please include a valid email"),
+  check("name")
+    .not().isEmpty().withMessage("Name is required")
+    .isAlphanumeric().withMessage("Name must be alphanumeric"),
+  check("email")
+    .isEmail().withMessage("Please include a valid email")
+    .normalizeEmail(),
   check("password")
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters long"),
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
+    .matches(/\d/).withMessage("Password must contain a number")
+    .matches(/[A-Z]/).withMessage("Password must contain an uppercase letter")
+    .matches(/[a-z]/).withMessage("Password must contain a lowercase letter")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage("Password must contain a special character"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
