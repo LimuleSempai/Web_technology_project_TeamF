7216923e9b14fdba7f4e8ca5b001d7fb7e87b509
LimuleSempai <125760323+LimuleSempai@users.noreply.github.com>
2025-03-16 21:03:50 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
 
 require("dotenv").config();
 connectDB();
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes.js"));
 app.use("/api/transport", require("./routes/transportRoutes.js"));
